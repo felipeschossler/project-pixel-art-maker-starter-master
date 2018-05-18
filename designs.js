7a0ca8948bdbafbc7a5cc45b2ff576b601d56d34
Felipe Schossler <felipealvesschossler@gmail.com>
2018-05-17 22:08:13 -0300

    Tests and fix the structure

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+$(document).ready(function(){
 
-// When size is submitted by the user, call makeGrid()
+    //Declaration of variables
 
-function makeGrid() {
+    var colorPicker = $('#colorPicker');
+    var inputHeight = $('#inputHeight');
+    var inputWidht = $('#inputWidht');
+    var sizePicker = $('#sizePicker');
+    var pixelCanvas = $('#pixelCanvas');
 
-// Your code goes here!
+    //function that reads the value of the inputs
 
-}
+    $(sizePicker).submit(function(){
+        colorPicker = $(colorPicker).val();
+        inputHeight = $(inputHeight).val();
+        inputWidht = $(inputWidht).val();
+        
+
+        $(pixelCanvas).add('tr').add('td');
+        
+        return false;
+    });
+    
+    function makeGrid() {
+    }
+
+
+
+
+});
+
