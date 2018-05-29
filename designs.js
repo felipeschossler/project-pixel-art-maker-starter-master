a3c2ddbd8210a8da8860d226c611e3e8ba0d3713
Felipe Alves <felipes@matriz.sicred.com>
2018-05-29 10:35:27 -0300

    I add preventDefault() and i declare the variables for loop

--- designs.js
+++ designs.js
@@ -1,38 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    //Declaration of variables
+    //Declaration of variables that will use in grid
 
     var colorPicker = $('#colorPicker');
     var inputHeight = $('#inputHeight');
     var inputWidht = $('#inputWidht');
-    var sizePicker = $('#sizePicker');
-    var pixelCanvas = $('#pixelCanvas');
-
+    
     //function that reads the value of the inputs
 
-    function makeGrid(){
-    
-        $(sizePicker).submit(function(){
-
-            colorPicker = $(colorPicker).val();
-            inputHeight = $(inputHeight).val();
-            inputWidht = $(inputWidht).val();
-            
-            //loop for Height and Widht
-            for(cont = 1; cont <= inputHeight; cont++){
-                addTr = $(pixelCanvas).append('<tr></tr>');
-                for(cont1 = 1; cont1 <= inputWidht; cont1++){
-                    addTd = $('tr:last').append('<td></td>');
-                    $('td').click(function() {
-                        $(this).css("background-color", colorPicker);
-                    });
-                }
-            }
-            
-            return false;
-        });
-    }    
+    $("#sizePicker").submit(function () {
         
-    makeGrid();
-});
 
+        event.preventDefault(); //prevent the page to execute the default action, that in this case is to reload
+        colorPicker = $("#colorPicker").val();
+        inputHeight = $("#inputHeight").val();
+        inputWidht = $("#inputWidht").val();
+
+        $("#pixelCanvas").empty(); //remove all children from `pixelCanvas` before the loop for re-submit
+        let cont, cont1; //declaring the variables used on the `for` command
+        //loop for Height and Widht
+        for (cont = 1; cont <= inputHeight; cont++) {
+            addTr = $("#pixelCanvas").append('<tr></tr>');
+            for (cont1 = 1; cont1 <= inputWidht; cont1++) {
+                addTd = $('tr:last').append('<td></td>');
+                $('td').click(function () {
+                    $(this).css("background-color", colorPicker);
+                });
+            }
+        }
+
+
+    });
+});
