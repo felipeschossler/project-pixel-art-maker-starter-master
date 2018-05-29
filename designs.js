0d899c051c918fbc15fd36503af510bc09e26cc6
Felipe Schossler <felipealvesschossler@gmail.com>
2018-05-28 22:52:50 -0300

    I put the function submit in a function makeGrid()

--- designs.js
+++ designs.js
@@ -10,25 +10,29 @@ $(document).ready(function(){
 
     //function that reads the value of the inputs
 
-    $(sizePicker).submit(function(){
-
-        colorPicker = $(colorPicker).val();
-        inputHeight = $(inputHeight).val();
-        inputWidht = $(inputWidht).val();
-        
-        //loop for Height and Widht
-        for(cont = 1; cont <= inputHeight; cont++){
-            addTr = $(pixelCanvas).append('<tr></tr>');
-            for(cont1 = 1; cont1 <= inputWidht; cont1++){
-                addTd = $('tr:last').append('<td></td>');
-                $('td').click(function() {
-                    $(this).css("background-color", colorPicker);
-                });
-            }
-        }
-        
-        return false;
-    });
+    function makeGrid(){
     
+        $(sizePicker).submit(function(){
+
+            colorPicker = $(colorPicker).val();
+            inputHeight = $(inputHeight).val();
+            inputWidht = $(inputWidht).val();
+            
+            //loop for Height and Widht
+            for(cont = 1; cont <= inputHeight; cont++){
+                addTr = $(pixelCanvas).append('<tr></tr>');
+                for(cont1 = 1; cont1 <= inputWidht; cont1++){
+                    addTd = $('tr:last').append('<td></td>');
+                    $('td').click(function() {
+                        $(this).css("background-color", colorPicker);
+                    });
+                }
+            }
+            
+            return false;
+        });
+    }    
+        
+    makeGrid();
 });
 
